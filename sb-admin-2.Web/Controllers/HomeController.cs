3f5417fd4ab80a8a2f28fea5b2047af5b7f48d26
earvintulagan <75780241+earvintulagan@users.noreply.github.com>
2021-01-21 21:45:43 +0800

    Add project files.

---
+++ sb-admin-2.Web/Controllers/HomeController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sb_admin_2.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FlotCharts()
+        {
+            return View("FlotCharts");
+        }
+
+        public ActionResult MorrisCharts()
+        {
+            return View("MorrisCharts");
+        }
+
+        public ActionResult Tables()
+        {
+            return View("Tables");
+        }
+
+        public ActionResult Forms()
+        {
+            return View("Forms");
+        }
+
+        public ActionResult Panels()
+        {
+            return View("Panels");
+        }
+
+        public ActionResult Buttons()
+        {
+            return View("Buttons");
+        }
+
+        public ActionResult Notifications()
+        {
+            return View("Notifications");
+        }
+
+        public ActionResult Typography()
+        {
+            return View("Typography");
+        }
+
+        public ActionResult Icons()
+        {
+            return View("Icons");
+        }
+
+        public ActionResult Grid()
+        {
+            return View("Grid");
+        }
+
+        public ActionResult Blank()
+        {
+            return View("Blank");
+        }
+
+        public ActionResult Login()
+        {
+            return View("Login");
+        }
+
+
+        public JsonResult GetEvents()
+        {
+            using (RegistrationEntities dc = new RegistrationEntities())
+            {
+                var events = dc.Calendars.ToList();
+                return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
+
+        [HttpPost]
+        public JsonResult SaveEvent(Calendar e)
+        {
+            var status = false;
+            using (RegistrationEntities dc = new RegistrationEntities())
+            {
+                if (e.EventID > 0)
+                {
+                    //Update the event
+                    var v = dc.Calendars.Where(a => a.EventID == e.EventID).FirstOrDefault();
+                    if (v != null)
+                    {
+                        v.Subject = e.Subject;
+                        v.Start = e.Start;
+                        v.End = e.End;
+                        v.Description = e.Description;
+                        v.IsFullDay = e.IsFullDay;
+                        v.ThemeColor = e.ThemeColor;
+                    }
+                }
+                else
+                {
+                    dc.Calendars.Add(e);
+                }
+
+                dc.SaveChanges();
+                status = true;
+
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+
+        [HttpPost]
+        public JsonResult DeleteEvent(int eventID)
+        {
+            var status = false;
+            using (RegistrationEntities dc = new RegistrationEntities())
+            {
+                var v = dc.Calendars.Where(a => a.EventID == eventID).FirstOrDefault();
+                if (v != null)
+                {
+                    dc.Calendars.Remove(v);
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+    }
+}
